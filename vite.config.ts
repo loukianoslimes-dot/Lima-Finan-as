07aec0dda4fd17ac42c532e82ee21ec687ce4ba2
loukianoslimes-dot <loukianoslimes@gmail.com>
2026-04-16 23:11:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';base: mode === 'production' ? '/Lima-Finan-as/' : '/',
+import {defineConfig, loadEnv} from 'vite';
+base: mode === 'production' ? '/Lima-Finan-as/' : '/',
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
